fe4f28b22f9fdca13c70740364602e5e2aec6198
Rares Musea <rares.musea@unriskomega.com>
2025-12-11 12:52:17 +0200

    Add basic landing hero section (only UI)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import {Hero} from "@/components/specific/landing/Hero";
+import { Hero } from '@/components/specific/landing/Hero';
 
 export default function Home() {
     return (
-        <div
-            className="relative min-h-screen flex items-center justify-center px-4 py-20 overflow-hidden bg-background text-foreground">
-            <Hero/>
-        </div>
+        <>
+            <div className="relative min-h-screen flex items-center justify-center px-4 py-20 overflow-hidden bg-background text-foreground">
+                <Hero />
+            </div>
+        </>
     );
 }
