5b967716593498db019c58923984d3a81dcaddbd
Rares Musea <rares.musea@unriskomega.com>
2025-12-18 11:13:12 +0200

    Move utilitary functions to the lib file

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateSlug(length = 6): string {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let slug: string = '';
+
+    for (let i = 0; i < length; i++) {
+        slug += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return slug;
+}
