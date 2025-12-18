515c64c74d99b5eb0d7969fe6fff347d576581a2
Rareș-Gabriel Mușea <77101390+RaresMusea@users.noreply.github.com>
2025-12-18 18:20:58 +0200

    Provide frontend logic for shortening out a link (#20)

--- dal/links/links.types.ts
+++ dal/links/links.types.ts
@@ -1,3 +1,5 @@
+import { hasNullableOrUndefinedString, hasString, isPlainObject } from '@/lib/guards';
+
 export type CreateLinkInput = {
     slug: string;
     targetUrl: string;
@@ -8,5 +10,22 @@ export type CreatedLink = {
     id: string;
     slug: string;
     targetUrl: string;
-    ownerId: string | null;
+    ownerId?: string | null;
 };
+
+export type CreatedLinkResponse = {
+    created: CreatedLink;
+    shortUrl: string;
+};
+
+export function isCreatedLink(x: unknown): x is CreatedLink {
+    if (!isPlainObject(x)) return false;
+
+    return hasString(x, 'id') && hasString(x, 'targetUrl') && hasNullableOrUndefinedString(x, 'ownerId');
+}
+
+export function isCreatedLinkResponse(x: unknown): x is CreatedLinkResponse {
+    if (!isPlainObject(x)) return false;
+
+    return 'created' in x && isCreatedLink(x.created) && hasString(x, 'shortUrl');
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { isPlainObject } from '@/lib/guards';
+import { ApiRouteResponse } from '@/lib/types';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
 
 export function generateSlug(length = 6): string {
@@ -14,4 +16,13 @@ export function generateSlug(length = 6): string {
     }
 
     return slug;
+}
+
+export function isApiRouteResponseOf<T>(x: unknown, isData: (v: unknown) => v is T): x is ApiRouteResponse<T> {
+    if (!isPlainObject(x)) return false;
+    if (typeof x.success !== 'boolean') return false;
+
+    if (x.success) return 'data' in x && isData(x.data);
+
+    return 'error' in x && typeof x.error === 'string' && (x.status === undefined || typeof x.status === 'number');
 }
