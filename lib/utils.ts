a95571ee8c5b0690c51576d4da0d24c04e3ee9fd
Rares Musea <rares.musea@unriskomega.com>
2026-01-13 11:03:48 +0200

    Remove duplicate function entry

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,17 +7,6 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export function generateSlug(length = 6): string {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let slug: string = '';
-
-    for (let i = 0; i < length; i++) {
-        slug += chars[Math.floor(Math.random() * chars.length)];
-    }
-
-    return slug;
-}
-
 export function isApiRouteResponseOf<T>(x: unknown, isData: (v: unknown) => v is T): x is ApiRouteResponse<T> {
     if (!isPlainObject(x)) return false;
     if (typeof x.success !== 'boolean') return false;
