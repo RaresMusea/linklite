edd5589f91aa423cbc42bf0ddf0b68ce04e5e65b
Rares Musea <rares.musea@unriskomega.com>
2026-01-19 16:55:00 +0200

    Make prisma singleton

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,9 +2,21 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from 'prisma/config';
 import { PrismaClient } from '@/generated/prisma/client';
 
-const connectionString = `${env("DATABASE_URL")}`;
-
+const connectionString = env('DATABASE_URL');
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+type GlobalWithPrisma = typeof globalThis & {
+    __prisma?: PrismaClient;
+};
+
+const g = globalThis as GlobalWithPrisma;
+
+export const prisma =
+    g.__prisma ??
+    new PrismaClient({
+        adapter,
+    });
+
+if (process.env.NODE_ENV !== 'production') {
+    g.__prisma = prisma;
+}
