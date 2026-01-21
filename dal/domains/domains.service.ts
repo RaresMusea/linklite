312f58c711e5f8f34a0cf3394eb9b6b45bcc3430
Rares Musea <rares.musea@unriskomega.com>
2026-01-21 08:33:07 +0200

    Update domains.service.ts

--- dal/domains/domains.service.ts
+++ dal/domains/domains.service.ts
@@ -1,8 +1,8 @@
-import { RdapDomainParams, RdapStatus } from '@/dal/domains/domains.types';
 import { getRegistrableDomain } from '@/lib/utils';
 import { extractRegistrationDate, fetchRdapJson, getRdapUrl, isRedactedRegistration } from '@/lib/rdap/rdap.endpoints';
-import { FetchRdapInfoResponse } from '@/lib/rdap/rdap.types';
+import { FetchRdapInfoResponse, RdapDomainParams, RdapStatus } from '@/lib/rdap/rdap.types';
 import { Prisma } from '@/generated/prisma/client';
+import { WhoisDomainParams, WhoisStatus } from '@/lib/whois/whois.types';
 
 export async function getRdapInfo(host: string): Promise<RdapDomainParams> {
     const now = new Date();
@@ -10,23 +10,23 @@ export async function getRdapInfo(host: string): Promise<RdapDomainParams> {
     const domain = getRegistrableDomain(host) ?? null;
 
     if (!domain) {
-        return getDomainParams(now, RdapStatus.UNSUPPORTED);
+        return getRdapDomainParams(now, RdapStatus.UNSUPPORTED);
     }
 
     const url = getRdapUrl(domain);
 
     if (!url) {
-        return getDomainParams(now, RdapStatus.UNSUPPORTED);
+        return getRdapDomainParams(now, RdapStatus.UNSUPPORTED);
     }
 
     const fetched: FetchRdapInfoResponse = await fetchRdapJson(url);
 
     if (!fetched.ok) {
         if (fetched.status === 404) {
-            return getDomainParams(now, RdapStatus.MISSING, now);
+            return getRdapDomainParams(now, RdapStatus.MISSING, now);
         }
 
-        return getDomainParams(now, RdapStatus.ERROR, now);
+        return getRdapDomainParams(now, RdapStatus.ERROR, now);
     }
 
     const registeredAt = extractRegistrationDate(fetched.json);
@@ -39,12 +39,24 @@ export async function getRdapInfo(host: string): Promise<RdapDomainParams> {
     }
 
     return {
-        ...getDomainParams(now, status, now, registeredAt),
+        ...getRdapDomainParams(now, status, now, registeredAt),
         rdapRaw: fetched.json as Prisma.InputJsonValue,
     };
 }
 
-function getDomainParams(
+// export async function getWhoisInfo(hostname: string): Promise<WhoisDomainParams> {
+//     const now = new Date();
+//
+//     const domain = getRegistrableDomain(hostname) ?? null;
+//
+//     if (!domain) {
+//         return getWhoisDomainParams(now, WhoisStatus.UNSUPPORTED);
+//     }
+//
+//     const url
+// }
+
+function getRdapDomainParams(
     date: Date,
     status: RdapStatus,
     rdapFetchedAt?: Date,
@@ -56,5 +68,20 @@ function getDomainParams(
         source: 'RDAP',
         checkedAt: date,
         rdapFetchedAt,
+    };
+}
+
+function getWhoisDomainParams(
+    date: Date,
+    status: WhoisStatus,
+    whoisFetchedAt?: Date,
+    registeredAt: Date | null = null
+): WhoisDomainParams {
+    return {
+        registeredAt,
+        status,
+        source: 'WHOIS',
+        checkedAt: date,
+        whoisFetchedAt,
     };
 }
