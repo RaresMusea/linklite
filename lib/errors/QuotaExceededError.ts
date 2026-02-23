6d375b15064332b3bbbb6ac0d9b9efd9901a2af4
Rares Musea <rares.musea@unriskomega.com>
2026-02-23 13:14:59 +0200

    Add custom quota exceeded error

---
+++ lib/errors/QuotaExceededError.ts
@@ -0,0 +1,14 @@
+import { AppError } from '@/lib/errors/AppError';
+
+export class QuotaExceededError extends AppError {
+  constructor(limit?: number) {
+    super(
+      'QUOTA_EXCEEDED',
+      limit
+        ? `You've reached the anonymous limit (${limit}). Sign up for free to create more short links.`
+        : "You've reached the anonymous limit. Sign up for free to create more short links.",
+      429,
+      true
+    );
+  }
+}
