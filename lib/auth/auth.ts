d7ef00915b457c4c87ba8af6a5b3e2e230cc5b1e
Rares Musea <rares.musea@unriskomega.com>
2026-03-09 12:45:02 +0200

    Add auth tables. Configure better-auth for Prisma ORM

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,8 +1,14 @@
 import { betterAuth } from 'better-auth';
+import { prismaAdapter } from '@better-auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
 
 const isProd = process.env.NODE_ENV === 'production'; // TODO move this logic to shared lib
 
 export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: 'postgresql',
+    }),
+
     baseURL: process.env.BETTER_AUTH_URL!,
     secret: process.env.BETTER_AUTH_SECRET!,
 
@@ -11,4 +17,6 @@ export const auth = betterAuth({
     },
 
     advanced: { defaultCookieAttributes: { secure: isProd, sameSite: 'lax', httpOnly: true } },
+
+    
 });
