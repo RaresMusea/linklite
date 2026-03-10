2da5ab99295e5caeff23df7528d6d5851fcfa65b
Rares Musea <rares.musea@unriskomega.com>
2026-03-10 15:57:05 +0200

    Provide auth hook for sending verification emails after registration

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from '@better-auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
+import { accountVerificationTemplate } from '@/lib/email/templates/account_verification';
+import { sendEmail } from '@/lib/email/send_email';
 
 const isProd = process.env.NODE_ENV === 'production'; // TODO move this logic to shared lib
 
@@ -11,12 +13,26 @@ export const auth = betterAuth({
 
     baseURL: process.env.BETTER_AUTH_URL!,
     secret: process.env.BETTER_AUTH_SECRET!,
+    trustedOrigins: process.env.TRUSTED_ORIGINS!.split(','),
 
     emailAndPassword: {
         enabled: true,
     },
+    emailVerification: {
+        sendOnSignUp: true,
+        sendVerificationEmail: async ({ user, url }) => {
+            const template = accountVerificationTemplate({
+                name: user.name,
+                verificationUrl: url,
+            });
 
+            await sendEmail({
+                to: user.email,
+                subject: template.subject,
+                html: template.html,
+                text: template.text,
+            });
+        },
+    },
     advanced: { defaultCookieAttributes: { secure: isProd, sameSite: 'lax', httpOnly: true } },
-
-    
 });
